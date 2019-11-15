492fb3b9bd6f533d54e4b4db9443d2922ba34732
Rasha Moumneh <rasha@fastmail.com>
2019-11-15 11:21:14 -0500

    Create login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,6 @@
 import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
@@ -6,7 +8,26 @@ const Login = () => {
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <h2>Log In</h2>
+      <form>
+        <TextField
+          id="outlined-username-input"
+          label="Username"
+          type="username"
+          margin="normal"
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          margin="normal"
+          variant="outlined"
+        />
+        <Button variant="contained" color="primary">
+          Log In
+        </Button>
+      </form>
     </>
   );
 };
